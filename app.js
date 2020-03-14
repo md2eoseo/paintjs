311d4e25ed45287aaebb559749d6082386ed21ec
md2eoseo <md2eoseo@gmail.com>
2020-03-14 11:00:57 +0100

    added drawing black line

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+
+// pixel modifier에 size 지정해야 그릴 수 있음
+// css에 size 지정하는건 보여지는 element의 크기임
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,22 +15,26 @@ function stopPainting() {
   painting = false;
 }
 
-function onMouseMove(e) {
-  const x = e.offsetX;
-  const y = e.offsetY;
-}
-
-function onMouseDown(e) {
+function startPainting() {
   painting = true;
 }
 
-function onMouseUp(e) {
-  stopPainting();
+function onMouseMove(e) {
+  const x = e.offsetX;
+  const y = e.offsetY;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
