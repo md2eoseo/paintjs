ba64d84b4c58803d2e519b5a1a0c4f22f5d2fec0
md2eoseo <md2eoseo@gmail.com>
2020-03-14 10:28:22 +0100

    added eventlistener

---
+++ app.js
@@ -0,0 +1,27 @@
+const canvas = document.querySelector("#jsCanvas");
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function onMouseMove(e) {
+  const x = e.offsetX;
+  const y = e.offsetY;
+}
+
+function onMouseDown(e) {
+  painting = true;
+}
+
+function onMouseUp(e) {
+  stopPainting();
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
